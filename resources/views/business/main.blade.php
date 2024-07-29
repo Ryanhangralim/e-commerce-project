f34cd1096af37789e8323166478b598d9ae1a3b0
Ryanhangralim <ryanhangralim@gmail.com>
2024-07-29 18:02:45 +0800

    Init main page for business

---
+++ resources/views/business/main.blade.php
@@ -0,0 +1,5 @@
+Business name = {{ $business->name }}
+<p></p>
+Business description = {{ $business->description }}
+<p></p>
+Business username = {{ $business->user->first_name }} {{ $business->user->last_name }}
