9e3b22a27da5b021b2e66e05eac1084966e6e0ea
Ryanhangralim <ryanhangralim@gmail.com>
2024-08-13 14:17:04 +0800

    Add product factory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $business_id = range(1, 2);
+        $category_id = range(1, 7);
+        $price = range(1000, 100000000, 1000);
+        $stock = range(0, 10);
+        $discount = range(0, 10);
+
+        return [
+            //
+            'business_id' => fake()->randomElement($business_id),
+            'name' => implode(' ', fake()->unique()->words(mt_rand(1, 4))),
+            'description' => fake()->sentence(mt_rand(3, 9)),
+            'brand' => ucwords(fake()->firstName()),
+            'category_id' => fake()->randomElement($category_id),
+            'price' => fake()->randomElement($price),
+            'stock' => fake()->randomElement($stock),
+            'discount' => fake()->randomElement($discount)
+        ];
+    }
+}
